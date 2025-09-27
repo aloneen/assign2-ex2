d2f9d7081d00a7902c4553376afaf2c3b07ed0cc
aloneen <seisenbek2005@mail.ru>
2025-09-27 10:38:58 +0500

    Final step. All requirements are satisfied

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # assign2-ex2
+
+
+### Exercise 2: Working with PostgreSQL using GORM
+#### Objective: Use GORM to perform similar operations as above, but with an ORM approach.
+##### 1.	Setup GORM: Install GORM and the PostgreSQL driver:
+##### 2.	Create a Model: Define the User model that maps to the users table.
+##### 3.	Auto Migrate: Use GORM’s AutoMigrate to create the users table based on the User struct.
+##### 4.	Insert Data: Use GORM to insert users into the database.
+##### 5.	Query Data: Use GORM to retrieve users from the database.
--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
-import "github.com/aloneen/assign2-ex2/initializers"
+import (
+	"fmt"
+	"log"
+
+	"github.com/aloneen/assign2-ex2/initializers"
+	"github.com/aloneen/assign2-ex2/models"
+)
 
 func init() {
-    initializers.LoadEnvVariable()
-    initializers.ConnectToDatabase()
+	initializers.LoadEnvVariable()
+	initializers.ConnectToDatabase()
 }
 
 func main() {
-	
+
+	//  Insert users
+	initializers.DB.Create(&models.User{Name: "Dias", Age: 22})
+	initializers.DB.Create(&models.User{Name: "Aigerim", Age: 25})
+	initializers.DB.Create(&models.User{Name: "Rustem", Age: 30})
+	fmt.Println("Inserted sample users")
+
+	//  Query users
+	var users []models.User
+	result := initializers.DB.Find(&users)
+	if result.Error != nil {
+		log.Fatal("❌ Query failed:", result.Error)
+	}
+
+	fmt.Println("Users in DB: ")
+	for _, u := range users {
+		fmt.Printf("ID: %d | Name: %s | Age: %d\n", u.ID, u.Name, u.Age)
+	}
+
 }
